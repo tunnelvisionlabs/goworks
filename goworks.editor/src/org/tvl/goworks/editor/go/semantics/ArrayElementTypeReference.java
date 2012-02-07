8dc4cbbc4a69a37a83c24f40c0415f35df48884f
Sam Harwell <sam@tunnelvisionlabs.com>
2012-02-07 16:53:45 -0600

    Implement ArrayElementTypeReference.resolve

--- goworks.editor/src/org/tvl/goworks/editor/go/semantics/ArrayElementTypeReference.java
+++ goworks.editor/src/org/tvl/goworks/editor/go/semantics/ArrayElementTypeReference.java
@@ -8,10 +8,19 @@
  */
 package org.tvl.goworks.editor.go.semantics;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import org.tvl.goworks.editor.go.codemodel.CodeElementModel;
 import org.tvl.goworks.editor.go.codemodel.PackageModel;
+import org.tvl.goworks.editor.go.codemodel.TypeAliasModel;
+import org.tvl.goworks.editor.go.codemodel.TypeArrayModel;
+import org.tvl.goworks.editor.go.codemodel.TypeMapModel;
+import org.tvl.goworks.editor.go.codemodel.TypeModel;
+import org.tvl.goworks.editor.go.codemodel.TypePointerModel;
+import org.tvl.goworks.editor.go.codemodel.TypeSliceModel;
+import org.tvl.goworks.editor.go.codemodel.VarModel;
 
 /**
  *
@@ -27,7 +36,49 @@ public class ArrayElementTypeReference extends CodeElementReference {
     
     @Override
     public Collection<? extends CodeElementModel> resolve(GoAnnotatedParseTree annotatedParseTree, PackageModel currentPackage, Map<String, Collection<PackageModel>> resolvedPackages) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Collection<? extends CodeElementModel> indexableType = arrayType.resolve(annotatedParseTree, currentPackage, resolvedPackages);
+        if (indexableType.isEmpty()) {
+            return indexableType;
+        }
+
+        List<CodeElementModel> resolvedTypes = new ArrayList<CodeElementModel>();
+        for (CodeElementModel model : indexableType) {
+            if (model instanceof VarModel) {
+                model = ((VarModel)model).getVarType();
+            }
+
+            if (model instanceof TypeModel) {
+                resolvedTypes.addAll(((TypeModel)model).resolve());
+            } else {
+                resolvedTypes.add(model);
+            }
+        }
+
+        Collection<CodeElementModel> elements = new ArrayList<CodeElementModel>();
+        for (CodeElementModel arrayModel : resolvedTypes) {
+            if (!(arrayModel instanceof TypeModel)) {
+                elements.add(arrayModel);
+                continue;
+            }
+
+            while (arrayModel instanceof TypeAliasModel) {
+                arrayModel = ((TypeAliasModel)arrayModel).getType();
+            }
+
+            if (arrayModel instanceof TypePointerModel && ((TypePointerModel)arrayModel).getElementType() instanceof TypeArrayModel) {
+                arrayModel = ((TypePointerModel)arrayModel).getElementType();
+            }
+
+            if (arrayModel instanceof TypeArrayModel) {
+                elements.add(((TypeArrayModel)arrayModel).getElementType());
+            } else if (arrayModel instanceof TypeSliceModel) {
+                elements.add(((TypeSliceModel)arrayModel).getElementType());
+            } else if (arrayModel instanceof TypeMapModel) {
+                elements.add(((TypeMapModel)arrayModel).getValueType());
+            }
+        }
+
+        return elements;
     }
 
 }
