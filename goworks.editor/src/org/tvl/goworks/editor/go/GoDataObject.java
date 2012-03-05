a7600a407d95485d3b9aa521a5232ccb223ce046
Sam Harwell <sam@tunnelvisionlabs.com>
2012-03-05 08:15:19 -0600

    Add GoDataLoader, clean up file type registration

--- goworks.editor/src/org/tvl/goworks/editor/go/GoDataObject.java
+++ goworks.editor/src/org/tvl/goworks/editor/go/GoDataObject.java
@@ -9,37 +9,47 @@
 package org.tvl.goworks.editor.go;
 
 import java.io.IOException;
+import org.netbeans.core.spi.multiview.MultiViewElement;
+import org.netbeans.core.spi.multiview.text.MultiViewEditorElement;
 import org.openide.filesystems.FileObject;
-import org.openide.loaders.DataNode;
+import org.openide.filesystems.MIMEResolver;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
-import org.openide.nodes.Children;
-import org.openide.nodes.CookieSet;
-import org.openide.nodes.Node;
-import org.openide.text.DataEditorSupport;
 import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
+import org.openide.windows.TopComponent;
+import org.tvl.goworks.editor.GoEditorKit;
 
-/**
- *
- * @author Sam Harwell
- */
+@NbBundle.Messages({
+    "GoResolver=Go Files",
+    "CTL_SourceTabCaption=&Source"
+})
+@MIMEResolver.ExtensionRegistration(
+    position=100,
+    displayName="#GoResolver",
+    extension="go",
+    mimeType=GoEditorKit.GO_MIME_TYPE)
 public class GoDataObject extends MultiDataObject {
 
-    @SuppressWarnings("LeakingThisInConstructor")
     public GoDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
         super(pf, loader);
-        CookieSet cookies = getCookieSet();
-        cookies.add((Node.Cookie) DataEditorSupport.create(this, getPrimaryEntry(), cookies));
+        registerEditor(GoEditorKit.GO_MIME_TYPE, true);
     }
 
     @Override
-    protected Node createNodeDelegate() {
-        return new DataNode(this, Children.LEAF, getLookup());
+    protected int associateLookup() {
+        return 1;
     }
 
-    @Override
-    public Lookup getLookup() {
-        return getCookieSet().getLookup();
+    @MultiViewElement.Registration(
+        displayName="#CTL_SourceTabCaption",
+        iconBase="",
+        persistenceType=TopComponent.PERSISTENCE_ONLY_OPENED,
+        preferredID="",
+        mimeType=GoEditorKit.GO_MIME_TYPE,
+        position=2000)
+    public static MultiViewEditorElement createMultiViewEditorElement(Lookup context) {
+        return new MultiViewEditorElement(context);
     }
 }
