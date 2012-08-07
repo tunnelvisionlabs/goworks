7ad677411fc11b0442b922ad2ead9dcfe25f1696
Sam Harwell <sam@tunnelvisionlabs.com>
2012-08-07 16:22:09 -0500

    GoCustomIndexer.index(...) waits until indexing completes and reports errors to ErrorsCache

--- goworks.editor/src/org/tvl/goworks/editor/go/parser/GoCustomIndexer.java
+++ goworks.editor/src/org/tvl/goworks/editor/go/parser/GoCustomIndexer.java
@@ -9,23 +9,38 @@
 package org.tvl.goworks.editor.go.parser;
 
 import java.net.URL;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.antlr.netbeans.editor.parsing.SyntaxError;
+import org.antlr.netbeans.editor.parsing.SyntaxErrorConvertor;
 import org.antlr.netbeans.editor.text.VersionedDocument;
 import org.antlr.netbeans.editor.text.VersionedDocumentUtilities;
 import org.antlr.netbeans.parsing.spi.ParseContext;
+import org.antlr.netbeans.parsing.spi.ParserData;
 import org.antlr.netbeans.parsing.spi.ParserTaskManager;
+import org.antlr.v4.runtime.misc.Tuple;
+import org.antlr.v4.runtime.misc.Tuple2;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.modules.parsing.spi.indexing.Context;
 import org.netbeans.modules.parsing.spi.indexing.CustomIndexer;
 import org.netbeans.modules.parsing.spi.indexing.CustomIndexerFactory;
+import org.netbeans.modules.parsing.spi.indexing.ErrorsCache;
 import org.netbeans.modules.parsing.spi.indexing.Indexable;
 import org.netbeans.modules.parsing.spi.indexing.PathRecognizerRegistration;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.URLMapper;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.tvl.goworks.editor.GoEditorKit;
 import org.tvl.goworks.editor.go.GoParserDataDefinitions;
+import org.tvl.goworks.editor.go.codemodel.FileModel;
 import org.tvl.goworks.project.GoProject;
 
 /**
@@ -44,6 +59,7 @@ public class GoCustomIndexer extends CustomIndexer {
     @Override
     protected void index(Iterable<? extends Indexable> files, Context context) {
         ParserTaskManager taskManager = Lookup.getDefault().lookup(ParserTaskManager.class);
+        Deque<Tuple2<Indexable, Future<ParserData<FileModel>>>> futures = new ArrayDeque<Tuple2<Indexable, Future<ParserData<FileModel>>>>();
         for (Indexable file : files) {
             try {
                 if (!GoEditorKit.GO_MIME_TYPE.equals(file.getMimeType())) {
@@ -57,13 +73,60 @@ public class GoCustomIndexer extends CustomIndexer {
 
                 VersionedDocument versionedDocument = VersionedDocumentUtilities.getVersionedDocument(fileObject);
                 ParseContext parseContext = new ParseContext(versionedDocument);
-                taskManager.scheduleData(parseContext, GoParserDataDefinitions.FILE_MODEL);
+                Future<ParserData<FileModel>> future = taskManager.scheduleData(parseContext, GoParserDataDefinitions.FILE_MODEL);
+                futures.add(Tuple.create(file, future));
                 if (context.isCancelled()) {
                     break;
                 }
             } catch (RuntimeException ex) {
                 LOGGER.log(Level.FINE, "An error occurred while indexing a document.", ex);
             }
+        }
+
+        while (!futures.isEmpty()) {
+            Tuple2<Indexable, Future<ParserData<FileModel>>> pair = futures.poll();
+            Future<ParserData<FileModel>> future = pair.getItem2();
+            if (future == null) {
+                ErrorsCache.setErrors(context.getRootURI(), pair.getItem1(), Collections.<SyntaxError>emptyList(), SyntaxErrorConvertor.INSTANCE);
+                continue;
+            }
+
+            ParserData<FileModel> parserData = null;
+            try {
+                parserData = future.get(0, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (ExecutionException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (TimeoutException ex) {
+                futures.add(pair);
+                continue;
+            }
+
+            Iterable<? extends SyntaxError> errors = null;
+            if (parserData != null) {
+                Future<ParserData<CompiledModel>> futureCompiledModelData = taskManager.getData(parserData.getSnapshot(), null, GoParserDataDefinitions.COMPILED_MODEL);
+
+                ParserData<CompiledModel> compiledModelData = null;
+                try {
+                    compiledModelData = futureCompiledModelData != null ? futureCompiledModelData.get() : null;
+                } catch (InterruptedException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (ExecutionException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+
+                CompiledModel model = compiledModelData != null ? compiledModelData.getData() : null;
+                if (model != null) {
+                    errors = model.getResult().getSyntaxErrors();
+                }
+            }
+
+            if (errors == null) {
+                errors = Collections.emptyList();
+            }
+
+            ErrorsCache.setErrors(context.getRootURI(), pair.getItem1(), errors, SyntaxErrorConvertor.INSTANCE);
         }
     }
 
