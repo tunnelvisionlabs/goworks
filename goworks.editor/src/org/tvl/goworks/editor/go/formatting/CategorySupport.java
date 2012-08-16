204d4ef5af3d6f3b460f103957821941410ee741
Sam Harwell <sam@tunnelvisionlabs.com>
2012-08-16 16:34:13 -0500

    Extended CategorySupport to support JToggleButton components

--- goworks.editor/src/org/tvl/goworks/editor/go/formatting/CategorySupport.java
+++ goworks.editor/src/org/tvl/goworks/editor/go/formatting/CategorySupport.java
@@ -8,11 +8,19 @@
  */
 package org.tvl.goworks.editor.go.formatting;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.prefs.Preferences;
 import javax.swing.JComponent;
 import javax.swing.JEditorPane;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.api.editor.settings.SimpleValueNames;
 import org.netbeans.modules.options.editor.spi.PreferencesCustomizer;
 import org.netbeans.modules.options.editor.spi.PreviewProvider;
@@ -25,9 +33,13 @@ import org.tvl.goworks.editor.GoEditorKit;
  *
  * @author Sam Harwell
  */
-public class CategorySupport implements PreviewProvider, PreferencesCustomizer {
+public class CategorySupport implements ActionListener, PreviewProvider, PreferencesCustomizer {
 
     public static final String OPTION_ID = "org.tvl.goworks.editor.go.formatting.FormatOptions.ID";
+
+    private static final int LOAD = 0;
+    private static final int STORE = 1;
+    private static final int ADD_LISTENERS = 2;
 
     private final String previewText;
 
@@ -36,6 +48,7 @@ public class CategorySupport implements PreviewProvider, PreferencesCustomizer {
 
     private final Preferences preferences;
 
+    private final List<JComponent> components = new LinkedList<JComponent>();
     private JEditorPane previewPane;
 
     protected CategorySupport(Preferences preferences, String id, JPanel panel, String previewText) {
@@ -43,10 +56,37 @@ public class CategorySupport implements PreviewProvider, PreferencesCustomizer {
         this.id = id;
         this.panel = panel;
         this.previewText = previewText != null ? previewText : Bundle.SAMPLE_Default();
+
+        // Scan the panel for its components
+        scan(panel, components);
+
+        // Load and hook up all the components
+        loadFrom(preferences);
+        addListeners();
+    }
+
+    protected void addListeners() {
+        scan(ADD_LISTENERS, null);
+    }
+
+    protected void loadFrom(Preferences preferences) {
+        scan(LOAD, preferences);
+    }
+
+    protected void storeTo(Preferences p) {
+        scan(STORE, p);
     }
 
     protected void notifyChanged() {
+        storeTo(preferences);
         refreshPreview();
+    }
+
+    // ActionListener implementation ---------------------------------------
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        notifyChanged();
     }
 
     // PreviewProvider methods -----------------------------------------------------
@@ -143,6 +183,85 @@ public class CategorySupport implements PreviewProvider, PreferencesCustomizer {
                 Exceptions.printStackTrace(ex);
                 return null;
             }
+        }
+    }
+
+    // Private methods -----------------------------------------------------
+
+    private void performOperation(int operation, JComponent jc, AbstractFormatOption option, Preferences p) {
+        switch(operation) {
+        case LOAD:
+            loadData(jc, option, p);
+            break;
+
+        case STORE:
+            storeData(jc, option, p);
+            break;
+
+        case ADD_LISTENERS:
+            addListener(jc);
+            break;
+        }
+    }
+
+    private void scan(int what, Preferences p ) {
+        for (JComponent jc : components) {
+            Object o = jc.getClientProperty(OPTION_ID);
+            if (o instanceof String) {
+                performOperation(what, jc, FormatOptions.getOption((String)o), p);
+            } else if (o instanceof String[]) {
+                for(String oid : (String[])o) {
+                    performOperation(what, jc, FormatOptions.getOption(oid), p);
+                }
+            } else if (o instanceof AbstractFormatOption) {
+                performOperation(what, jc, (AbstractFormatOption)o, p);
+            } else if (o instanceof AbstractFormatOption[]) {
+                for(AbstractFormatOption oid : (AbstractFormatOption[])o) {
+                    performOperation(what, jc, oid, p);
+                }
+            }
+        }
+    }
+
+    private void scan(Container container, List<JComponent> components) {
+        for (Component c : container.getComponents()) {
+            if (c instanceof JComponent) {
+                JComponent jc = (JComponent)c;
+                Object o = jc.getClientProperty(OPTION_ID);
+                if (o instanceof String || o instanceof String[] || o instanceof AbstractFormatOption || o instanceof AbstractFormatOption[]) {
+                    components.add(jc);
+                }
+            }
+
+            if (c instanceof Container) {
+                scan((Container)c, components);
+            }
+        }
+    }
+
+    /** Very smart method which tries to set the values in the components correctly
+     */
+    private void loadData( JComponent jc, AbstractFormatOption optionID, Preferences node ) {
+        if ( jc instanceof JToggleButton ) {
+            JToggleButton toggle = (JToggleButton)jc;
+            toggle.setSelected( ((BooleanFormatOption)optionID).getValue(node) );
+        }
+    }
+
+    private void storeData( JComponent jc, @NonNull AbstractFormatOption option, Preferences node ) {
+        if ( jc instanceof JToggleButton ) {
+            JToggleButton toggle = (JToggleButton)jc;
+            if (!option.equals(FormatOptions.expandTabToSpaces) && ((BooleanFormatOption)option).getDefaultValue() == toggle.isSelected())
+                node.remove(option.getName());
+            else
+                node.putBoolean(option.getName(), toggle.isSelected());
+        }
+    }
+
+    private void addListener( JComponent jc ) {
+        if ( jc instanceof JToggleButton ) {
+            JToggleButton toggle = (JToggleButton)jc;
+            toggle.addActionListener(this);
         }
     }
 
