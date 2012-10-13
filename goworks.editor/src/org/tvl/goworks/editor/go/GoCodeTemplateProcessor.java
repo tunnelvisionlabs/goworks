db29f85c396d6cd17c5052f2a1507aa2974dfdb2
Sam Harwell <sam@tunnelvisionlabs.com>
2012-10-13 02:50:32 -0500

    New Go code templates

---
+++ goworks.editor/src/org/tvl/goworks/editor/go/GoCodeTemplateProcessor.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2012 Sam Harwell, Tunnel Vision Laboratories LLC
+ *  All rights reserved.
+ * 
+ *  The source code of this document is proprietary work, and is not licensed for
+ *  distribution. For information about licensing, contact Sam Harwell at:
+ *      sam@tunnelvisionlabs.com
+ */
+package org.tvl.goworks.editor.go;
+
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.lib.editor.codetemplates.spi.CodeTemplateInsertRequest;
+import org.netbeans.lib.editor.codetemplates.spi.CodeTemplateParameter;
+import org.netbeans.lib.editor.codetemplates.spi.CodeTemplateProcessor;
+import org.netbeans.lib.editor.codetemplates.spi.CodeTemplateProcessorFactory;
+import org.tvl.goworks.editor.GoEditorKit;
+
+/**
+ *
+ * @author Sam Harwell
+ */
+public class GoCodeTemplateProcessor implements CodeTemplateProcessor {
+
+    private final CodeTemplateInsertRequest _request;
+
+    public GoCodeTemplateProcessor(CodeTemplateInsertRequest request) {
+        this._request = request;
+    }
+
+    @Override
+    public void updateDefaultValues() {
+    }
+
+    @Override
+    public void parameterValueChanged(CodeTemplateParameter masterParameter, boolean typingChange) {
+    }
+
+    @Override
+    public void release() {
+    }
+
+    @MimeRegistration(mimeType=GoEditorKit.GO_MIME_TYPE, service=CodeTemplateProcessorFactory.class)
+    public static final class Factory implements CodeTemplateProcessorFactory {
+
+        @Override
+        public CodeTemplateProcessor createProcessor(CodeTemplateInsertRequest request) {
+            return new GoCodeTemplateProcessor(request);
+        }
+
+    }
+
+}
