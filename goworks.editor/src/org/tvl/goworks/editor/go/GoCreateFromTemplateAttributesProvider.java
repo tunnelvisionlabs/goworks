c413f5a95f536560abea03e0a64624a3608910ac
Sam Harwell <sam@tunnelvisionlabs.com>
2012-10-23 18:48:27 -0500

    Provide implementation of CreateFromTemplateAttributesProvider to provide the "package" attribute for Go templates

---
+++ goworks.editor/src/org/tvl/goworks/editor/go/GoCreateFromTemplateAttributesProvider.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2012 Sam Harwell, Tunnel Vision Laboratories LLC
+ *  All rights reserved.
+ * 
+ *  The source code of this document is proprietary work, and is not licensed for
+ *  distribution. For information about licensing, contact Sam Harwell at:
+ *      sam@tunnelvisionlabs.com
+ */
+package org.tvl.goworks.editor.go;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.openide.loaders.CreateFromTemplateAttributesProvider;
+import org.openide.loaders.DataFolder;
+import org.openide.loaders.DataObject;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Sam Harwell
+ */
+@ServiceProvider(service=CreateFromTemplateAttributesProvider.class)
+public class GoCreateFromTemplateAttributesProvider implements CreateFromTemplateAttributesProvider {
+
+    @Override
+    public Map<String, ?> attributesFor(DataObject template, DataFolder target, String name) {
+        if (!(template instanceof GoDataObject)) {
+            return null;
+        }
+
+        Map<String, String> attributes = new HashMap<String, String>();
+        attributes.put("package", target.getName());
+        return attributes;
+    }
+
+}
