24918e95a3083a4f8f4323cbf18fc68bae87b674
Sam Harwell <sam@tunnelvisionlabs.com>
2012-10-23 21:21:41 -0500

    Use @StaticResource annotation

--- goworks.editor/src/org/tvl/goworks/editor/go/navigation/GoNode.java
+++ goworks.editor/src/org/tvl/goworks/editor/go/navigation/GoNode.java
@@ -12,9 +12,41 @@ import java.awt.Image;
 import org.antlr.netbeans.editor.navigation.Description;
 import org.antlr.netbeans.editor.navigation.NavigatorNode;
 import org.antlr.netbeans.editor.navigation.NavigatorPanelUI;
+import org.netbeans.api.annotations.common.StaticResource;
 import org.openide.util.ImageUtilities;
 
 public class GoNode extends NavigatorNode {
+
+    @StaticResource
+    private static final String CONST_STATIC_PUBLIC = "org/tvl/goworks/editor/go/resources/const_static_public.png";
+    @StaticResource
+    private static final String CONST_STATIC_PROTECTED = "org/tvl/goworks/editor/go/resources/const_static_protected.png";
+    @StaticResource
+    private static final String CONST_PUBLIC = "org/tvl/goworks/editor/go/resources/const_public.png";
+    @StaticResource
+    private static final String CONST_PROTECTED = "org/tvl/goworks/editor/go/resources/const_protected.png";
+    @StaticResource
+    private static final String FIELD_STATIC_PUBLIC = "org/tvl/goworks/editor/go/resources/field_static_16.png";
+    @StaticResource
+    private static final String FIELD_STATIC_PROTECTED = "org/tvl/goworks/editor/go/resources/field_static_protected_16.png";
+    @StaticResource
+    private static final String FIELD_PUBLIC = "org/tvl/goworks/editor/go/resources/fields.png";
+    @StaticResource
+    private static final String FIELD_PROTECTED = "org/tvl/goworks/editor/go/resources/field_protected_16.png";
+    @StaticResource
+    private static final String METHOD_STATIC_PUBLIC = "org/tvl/goworks/editor/go/resources/methods_static.png";
+    @StaticResource
+    private static final String METHOD_STATIC_PROTECTED = "org/tvl/goworks/editor/go/resources/methods_static_protected.png";
+    @StaticResource
+    private static final String METHOD_PUBLIC = "org/tvl/goworks/editor/go/resources/methods.png";
+    @StaticResource
+    private static final String METHOD_PROTECTED = "org/tvl/goworks/editor/go/resources/methods_protected.png";
+    @StaticResource
+    private static final String STRUCT = "org/tvl/goworks/editor/go/resources/struct_16.png";
+    @StaticResource
+    private static final String INTERFACE = "org/tvl/goworks/editor/go/resources/interface.png";
+    @StaticResource
+    private static final String TYPEDEF = "org/tvl/goworks/editor/go/resources/typedef_16.png";
 
     public GoNode(NavigatorPanelUI ui, DeclarationDescription description) {
         super(ui, description, DeclarationNodeFactory.INSTANCE);
@@ -27,65 +59,64 @@ public class GoNode extends NavigatorNode {
 
     @Override
     public Image getIcon(int type) {
-        String resourceLocation = "org/tvl/goworks/editor/go/resources/";
         String imageName;
         switch (getDescription().getKind()) {
         case CONSTANT:
             if (getDescription().isExported()) {
-                imageName = "const_static_public.png";
+                imageName = CONST_STATIC_PUBLIC;
             } else {
-                imageName = "const_static_protected.png";
+                imageName = CONST_STATIC_PROTECTED;
             }
             break;
 
         case VARIABLE:
             if (getDescription().isExported()) {
-                imageName = "field_static_16.png";
+                imageName = FIELD_STATIC_PUBLIC;
             } else {
-                imageName = "field_static_protected_16.png";
+                imageName = FIELD_STATIC_PROTECTED;
             }
             break;
 
         case FIELD:
             if (getDescription().isExported()) {
-                imageName = "fields.png";
+                imageName = FIELD_PUBLIC;
             } else {
-                imageName = "field_protected_16.png";
+                imageName = FIELD_PROTECTED;
             }
             break;
 
         case FUNCTION:
             if (getDescription().isExported()) {
-                imageName = "methods_static.png";
+                imageName = METHOD_STATIC_PUBLIC;
             } else {
-                imageName = "methods_static_protected.png";
+                imageName = METHOD_STATIC_PROTECTED;
             }
             break;
 
         case METHOD:
             if (getDescription().isExported()) {
-                imageName = "methods.png";
+                imageName = METHOD_PUBLIC;
             } else {
-                imageName = "methods_protected.png";
+                imageName = METHOD_PROTECTED;
             }
             break;
 
         case STRUCT:
-            imageName = "struct_16.png";
+            imageName = STRUCT;
             break;
 
         case INTERFACE:
-            imageName = "interface.png";
+            imageName = INTERFACE;
             break;
 
         case UNKNOWN:
         default:
             // TODO: use error image
-            imageName = "struct_16.png";
+            imageName = STRUCT;
             break;
         }
 
-        return ImageUtilities.loadImage(resourceLocation + imageName);
+        return ImageUtilities.loadImage(imageName);
     }
 
     public static class DeclarationDescription extends Description {
